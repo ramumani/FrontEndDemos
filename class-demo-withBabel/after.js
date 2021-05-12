0f13c6ea97abb827c9ded9e9695abba612f15a02
ramumani <ramu.mani@hotmail.com>
2021-05-12 11:19:04 +0300

    adding babel

---
+++ class-demo-withBabel/after.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var greeting = function greeting(name) {
+  console.log("Good morning ".concat(name));
+};
+
+greeting("Ramu");
---
+++ class-demo-withBabel/before.js
@@ -0,0 +1,5 @@
+const greeting = (name) => {
+  console.log(`Good morning ${name}`);
+};
+
+greeting("Ramu");
