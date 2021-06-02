0c8f25853034786c7d0ab99445be62ffaea8a963
ramumani <ramu.mani@hotmail.com>
2021-06-02 06:43:13 +0300

    updating with ES6/8 demos

--- js-object-spreadoperator-demo/sandbox.js
+++ js-object-spreadoperator-demo/sandbox.js
@@ -1,6 +1,8 @@
 let person = { name: "Bob", position: "developer" };
 
+console.log('person:'+person);
+
 let newPerson = { ...person, city: "dallas", position: "senior developer" };
 
-console.log(person);
-console.log(newPerson);
+console.log('person:'+person);
+console.log('newPerson:'+newPerson);
